888c8b97268f3310b16a75a690bce44cb96ad54d
ninteo <hnt510@gmail.com>
2014-01-16 23:29:53 +0800

    added somthing...

--- gen/org/ninto/garagemgr/R.java
+++ gen/org/ninto/garagemgr/R.java
@@ -28,13 +28,13 @@ public final class R {
         public static final int action_settings=0x7f08000c;
         public static final int button1=0x7f08000a;
         public static final int button2=0x7f080009;
-        public static final int editText1=0x7f080006;
-        public static final int email=0x7f080003;
+        public static final int carNumber=0x7f080004;
         public static final int login=0x7f080005;
         public static final int login_form=0x7f080002;
         public static final int login_status=0x7f080000;
         public static final int login_status_message=0x7f080001;
-        public static final int password=0x7f080004;
+        public static final int name=0x7f080003;
+        public static final int phoneNumber=0x7f080006;
         public static final int sign_in_button=0x7f080007;
         public static final int textView1=0x7f080008;
     }
@@ -62,10 +62,11 @@ public final class R {
         public static final int error_invalid_password=0x7f05000e;
         public static final int hello_world=0x7f050002;
         public static final int login_progress_signing_in=0x7f05000c;
+        public static final int prompt_carNumber=0x7f050008;
         /**  Strings related to login 
          */
-        public static final int prompt_email=0x7f050007;
-        public static final int prompt_password=0x7f050008;
+        public static final int prompt_name=0x7f050007;
+        public static final int prompt_phoneNumber=0x7f050011;
         public static final int title_activity_login=0x7f050006;
         public static final int title_activity_logout=0x7f050005;
     }
---
+++ src/dao/SqlHelper.java
@@ -0,0 +1,50 @@
+package dao;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+class SqlHelper extends SQLiteOpenHelper {
+
+	private static final String DATABASE_NAME = "myDatabase.db";
+	private static final String DATABASE_TABLE = "GarageMgr";
+	private static final int DATABASE_VERSION = 1;
+	private static final String CAR_NUMBER = "CAR_NUMBER";
+	private static final String NAME = "NAME";
+	private static final String PHONE_NUMBER = "PHONE_NUMBER";
+	private static final String TIME = "TIME";
+	// SQL Statement to create a new database.
+	private static final String DATABASE_CREATE = "create table " +
+			DATABASE_TABLE + " (" + CAR_NUMBER +
+					" integer primary key, " +
+					NAME + " text not null, " +
+					PHONE_NUMBER + " integer, " +
+					TIME + " integer);";
+	public SqlHelper(Context context, String name,CursorFactory factory, int version) {
+			super(context, name, factory, version);
+		}
+					// Called when no database exists in disk and the helper class needs
+					// to create a new one.
+					@Override
+	public void onCreate(SQLiteDatabase db) {
+			db.execSQL(DATABASE_CREATE);
+		}
+					//Called when there is a database version mismatch meaning that
+					//the version of the database on disk needs to be upgraded to
+					//the current version.
+					@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion,int newVersion) {
+						//Log the version upgrade.
+			Log.w("TaskDBAdapter", "Upgrading from version " +
+				oldVersion + " to " +newVersion + ", which will destroy all old data");
+						//Upgrade the existing database to conform to the new
+						//version. Multiple previous versions can be handled by
+						//comparing oldVersion and newVersion values.
+						//The simplest case is to drop the old table and create a new one.
+						db.execSQL("DROP TABLE IF IT EXISTS " + DATABASE_TABLE);
+						//Create a new one.
+						onCreate(db);
+					}
+	}
